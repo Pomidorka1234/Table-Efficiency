e33d7eed1d2ead57ec6fd4a005d677f84c229634
Pomidorka1234 <Jansafronov@gmail.com>
2020-12-25 03:00:48 +0200

    commit on 'main'

--- Definitions.py
+++ Definitions.py
@@ -1,41 +1,76 @@
 class Matrix:
-    def __init__(self, height, width, data):
-        self.height = height
+    def __init__(self, width, height, rawData):
         self.width = width
+        self.height = height
+        self.raw = rawData
         self.matrix = []
         for i in range(self.height):
             self.matrix.append([])
             for j in range(self.width):
-                self.matrix[i].append(data[j + i * self.width])
-        self.raw = data
+                self.matrix[i].append(self.raw[j + i * self.width])
     
     #def mult(self, matrix):
         #if (self.width == matrix.height):
         #    return Matrix(self.height, matrix.width, )
 
+    def getVertex(self, row = -1, column = -1):
+        Pos = []
+        if column != -1:
+            for i in range(self.height):
+                Pos.append(self.matrix[i][column])
+            
+
+        if row != -1:
+            if Pos == []:
+                Pos = self.matrix[row]
+            else:
+                Pos = Pos[row]
+        return Pos
+
+
+
+    def __str__(self):
+        matrix = ''
+        for i in range(self.height):
+            for j in range(self.width):
+                cell = self.matrix[i][j]
+                if i == 0 and j == 0:
+                    space = 0
+                else:
+                    space = len(str(cell - 1)) - 1
+                    if len(str(cell - 1)) != len(str(cell)):
+                        space += 1
+                matrix += str(self.matrix[i][j]) + "  " + " " * (len(str(self.matrix[self.height - 1][self.width - 1])) - 1 - space)
+            matrix += '\n'
+        return matrix
+
 
 
 class Table:
-    def __init__(self, props):
-        self.label = { }
-        for i in range(len(props)):
-            self.label.update(props[i])
+    def __init__(self, width ,rawData = ["x0", "x1"]):
+        #try:
+            self.label = Matrix(width, int(len(rawData)/width), rawData)
+        #except:
+        #    print("Please provide correct input")
     
-    def sum(self, data = []):
-        if len(data) == 0:
-            data = self.label["cost"]
+    def sumVertex(self, vertex = ['column', 0]):
         sum = 0
-        for i in range(data.height * data.width):
-            sum += data.raw[i]
+        for i in self.label.getVertex(vertex[0] == vertex[1]):
+            sum += i
         return sum
 
-class BinaryTable(Table):
-    def __init__(self, cost, profit):
-        self.label = {
-            "cost": Matrix(len(cost), 1, cost),
-            "profit": Matrix(len(profit), 1, profit)
-        }
-        super().__init__([cost, profit])
+    def __str__(self):
+        table = ""
+        for i in range(self.label.height):
+            table += 'L' + str(i) + " | " + Matrix(self.label.width, 1, self.label.getVertex(i, -1)).__str__()
+        return table
+        
 
-    def Gamma(self, data):
-        return Matrix(self.sum(self.label["cost"]), 1, data)
+class BinaryTable(Table):
+
+    def __init__(self, rawData = [0, 0]):
+        super().__init__(2, rawData)
+
+
+    def Gamma(self, rawData):
+        return Matrix(1, self.label.height, rawData)
---
+++ testModule.py
@@ -0,0 +1,4 @@
+import Definitions as DFN
+
+tbl = DFN.BinaryTable([200, 50, 300, 70, 600, 100, 1000, 500, 2500, 650, 4000, 900])
+print(tbl.__str__())
