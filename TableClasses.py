d188606c096f9ada7fcf10744aa0e5e024be4bf8
Pomidorka1234 <Jansafronov@gmail.com>
2021-02-18 19:48:22 +0200

    fix

--- TableClasses.py
+++ TableClasses.py
@@ -1,7 +1,6 @@
 import BasicConcepts as BC
 
 class Matrix:
-
     """Matrix that is of a list * list type"""
 
     def __init__(self, width: int, height: int, rawData: list) -> None:
@@ -171,8 +170,8 @@ class Matrix:
 
 
 class Table:
-
     """Table that is of a list * matrix type"""
+
     def __init__(self, width: int ,rawData: list) -> None:
         """
         O(n) Initialize table's matrix
