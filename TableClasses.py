2558d62642b0ecafdb86f2d5c2bdc0bae69cfe0c
Pomidorka1234 <Jansafronov@gmail.com>
2021-02-18 20:09:12 +0200

    fix\

--- TableClasses.py
+++ TableClasses.py
@@ -1,11 +1,11 @@
 import BasicConcepts as BC
 
 class Matrix:
+    
     """Matrix that is of a list * list type"""
 
     def __init__(self, width: int, height: int, rawData: list) -> None:
         """
-        
         O(n*m) Initializes the matrix type
 
         Args:
